cd3a199ba4703f5b6e00778adfb86d21dda6e66d
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-14 17:39:16 +0200

    Testing navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<script setup>
+  import NavBar from './components/NavBar.vue';
+</script>
+
+
 <template>
+  <NavBar />
+  
+  
   <h1>Hello world</h1>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <div class="right-nav-bar-container">
+            <img src="@/assets/images/logo.jpg">
+        </div>
+        
+        <div class="left-nav-bar-container">
+            <i>Chi siamo</i>
+        </div>
+    </nav>
+</template>
