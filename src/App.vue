af6c53c8550f7074158a4bb4998074c936dd926c
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-15 12:41:56 +0200

    Played a bit with navbar and centering stuff in main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-  import NavBar from './components/Navbar.vue';
+    import NavBar from './components/Navbar.vue';
 </script>
 
 <template>
-  <NavBar />
-  <main>  
-    <h1>Hello world</h1>
-  </main>
+    <NavBar />
+    <main>  
+        <h1>Hello world</h1>
+    </main>
 
 </template>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,35 @@
 <template>
-    <nav>
+    <nav class="nav-container">
         <div class="right-nav-bar-container">
             <img src="@/assets/images/logo.jpg" class="nav-logo" alt="Logo for Web Design Project Website">
         </div>
         
         <div class="left-nav-bar-container">
-            <i>Chi siamo</i>
+            <i class="right-nav-second-button">Chi siamo</i>
         </div>
     </nav>
 </template>
 
 <style>
+    .nav-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background: black;
+        height: max-content;
+    }
+    
     .nav-logo {
         height: 50px;
         width: 50px;
+        padding: 5px;
+        border-radius: 100%;
+    }
+
+    i {
+        color: white;
+        padding-right: 10px;
+        background: linear-gradient();
     }
 
 </style>
