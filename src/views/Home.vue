074267527d0e40a5904e94144b5ec75b9c70351b
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-17 22:09:27 +0200

    Nuova immagine gatto bellissimo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,8 @@
     <div class="second-paragraph">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi odio sed accusantium facere, minus natus laboriosam quas quia illo nesciunt aliquam corporis. Ex nisi quas itaque, beatae molestias ratione praesentium.</p>
     </div>
+
+    <img src="/src/assets/images/la-guida-del-gattino1.jpg" class="immagine-gatto-bello" alt="Immagine di un gattino rossiccio patato lo voglio strapazzare">
 </template>
 
 <style scoped>
