ad56b9c2d5257979ad8d233572c1ac06cd757cc8
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-19 16:40:36 +0200

    Added logic for loading personal images

--- src/components/PictureContainer.vue
+++ src/components/PictureContainer.vue
@@ -28,17 +28,35 @@
 
 <template>
     <div class="picture-container">
-        <h1>{{ profile.firstName }} {{ profile.lastName }}</h1>
-        <img>
-        <p>{{ profile.description }}</p>
+        <h1 class="picture-container-firstname-and-lastname">{{ profile.firstName }} {{ profile.lastName }}</h1>
+        <img :src="`./src/assets/images/personal-pictures/${profile.photoLocation}`" class="picture-container-image" :alt="`An image of ${profile.firstName}`">
+        <p class="picture-container-description">{{ profile.description }}</p>
     </div>
 </template>
 
 <style scoped>
     .picture-container {
+        text-align: center;
+
+        border-color: black;
+        border-style: solid;
+        border-width: 5px;
+        border-radius: 20px;
+        margin-bottom: 20px;
+
         display: flex;
         flex-direction: column;
         justify-content: center;
         background-color: white;
     }
+
+    .picture-container-image {
+        height: 250px;
+        width: 250px;
+    }
+
+    .picture-container-description {
+        width: 350px;
+    }
+
 </style>
