2d7e90d0ab71f32a05cb208b8466e5360d1a809c
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-19 13:57:01 +0200

    WIP on ChiSiamo.vue

--- src/views/ChiSiamo.vue
+++ src/views/ChiSiamo.vue
@@ -8,11 +8,25 @@
     <div class="pictures-container">
         <pictureContainer ProfileName="Simone"/>
         <pictureContainer ProfileName="Thomas"/>
+        <pictureContainer ProfileName="Kevin"/>
+        <pictureContainer ProfileName="Loris"/>
+        <pictureContainer ProfileName="Gabriel"/>
+        <pictureContainer ProfileName="Giulia"/>
+        <pictureContainer ProfileName="Francesca"/>
+        <pictureContainer ProfileName="Lorenzo"/>
     </div>
 
 </template>
 
 <style scoped>
+    .pictures-container {
+        width: 80%;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        align-items: stretch;
+    }
+
     div.first-paragraph {
         width: 50%;
     }
