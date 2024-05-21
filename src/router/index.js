41e9f538829a7fae14fc65f3ca8f49edf574e124
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2024-05-22 00:18:06 +0200

    fixed terms of service button

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/views/Home.vue';
 import WhoWeAreView from '@/views/ChiSiamo.vue';
+import TermsofService from '@/views/TermsOfService.vue';
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/views/ChiSiamo.vue', component: WhoWeAreView },
+    { path: '/views/TermsOfService.vue', component: TermsofService },
 ]
 
 const router = createRouter ({
