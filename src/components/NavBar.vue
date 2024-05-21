1e0a96f6347c94553b54d05e813b251edd4aee0f
kevinslashgun <kevinbarragan1402@gmail.com>
2024-05-21 19:03:27 +0200

    chore: Update dependencies for Vue.js and Vuetify

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,27 @@
 <template>
-    <nav class="nav-container">
-        <div class="left-nav-bar-container">
-            <RouterLink to='/' class="a-container">
-                <img src="@/assets/images/logo.jpg" class="nav-logo" alt="Logo for Web Design Project Website">
-            </RouterLink>
-        </div>
-
-        <div class="center-nav-bar-container">
-            <button @click="chooseFirstPassport" class="center-nav-choose-first-passport-button">
-                Choose first passport
-            </button>
-
-            <img src="" class="center-nav-logotype" alt="Logotype for Web Design Project">
-
-            <button @click="chooseSecondPassport" class="center-nav-choose-second-passport-button">
-                Choose second passport
-            </button>
-        </div>
-
-        <div class="right-nav-bar-container">
-            <RouterLink to='/views/ChiSiamo.vue' class="right-nav-about-button">
-                About
-            </RouterLink>
-            <button @click="chooseLanguage" class="right-nav-choose-language-button">
-                Language
-            </button>
-        </div>
-    </nav>
-</template>
-
-<script>
-
-</script>
+    <div>
+      <MobileNavBar v-if="isMobile" />
+      <DesktopNavBar v-else />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted, onUnmounted } from 'vue'
+  import MobileNavBar from './MobileNavBar.vue'
+  import DesktopNavBar from './DesktopNavBar.vue'
+  
+  const isMobile = ref(window.innerWidth < 768)
+  
+  const handleResize = () => {
+    isMobile.value = window.innerWidth < 768
+  }
+  
+  onMounted(() => {
+    window.addEventListener('resize', handleResize)
+  })
+  
+  onUnmounted(() => {
+    window.removeEventListener('resize', handleResize)
+  })
+  </script>
+  
