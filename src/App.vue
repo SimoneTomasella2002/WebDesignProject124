ab1d530277827460bc4eccc8a979a0960550f200
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-21 18:10:44 +0200

    Palle

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,11 @@
 <script setup>
     import Footer from './components/Footer.vue';
+    import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
     <div class="app-container">
-        <nav class="nav-container">
-            <div class="left-nav-bar-container">
-                <RouterLink to='/' class="a-container">
-                    <img src="@/assets/images/logo.jpg" class="nav-logo" alt="Logo for Web Design Project Website">
-                </RouterLink>
-            </div>
-
-            <div class="center-nav-bar-container">
-                <button @click="chooseFirstPassport" class="center-nav-choose-first-passport-button">
-                    Choose first passport
-                </button>
-
-                <img src="" class="center-nav-logotype" alt="Logotype for Web Design Project">
-
-                <button @click="chooseSecondPassport" class="center-nav-choose-second-passport-button">
-                    Choose second passport
-                </button>
-            </div>
-
-            <div class="right-nav-bar-container">
-                <RouterLink to='/views/ChiSiamo.vue' class="right-nav-about-button">
-                    About
-                </RouterLink>
-                <button @click="chooseLanguage" class="right-nav-choose-language-button">
-                    Language
-                </button>
-            </div>
-        </nav>
+        <NavBar />
 
         <main>
             <RouterView />
---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="nav-container">
+            <div class="left-nav-bar-container">
+                <RouterLink to='/' class="a-container">
+                    <img src="@/assets/images/logo.jpg" class="nav-logo" alt="Logo for Web Design Project Website">
+                </RouterLink>
+            </div>
+
+            <div class="center-nav-bar-container">
+                <v-btn @click="chooseFirstPassport" class="center-nav-choose-first-passport-button">
+                    Choose first passport
+                </v-btn>
+
+                <img src="" class="center-nav-logotype" alt="Logotype for Web Design Project">
+
+                <v-btn @click="chooseSecondPassport" class="center-nav-choose-second-passport-button">
+                    Choose second passport
+                </v-btn>
+            </div>
+
+            <div class="right-nav-bar-container">
+                <RouterLink to='/views/ChiSiamo.vue' class="right-nav-about-button">
+                    About
+                </RouterLink>
+                <v-btn @click="chooseLanguage" class="right-nav-choose-language-button">
+                    Language
+                </v-btn>
+            </div>
+        </nav>
+</template>
+
+<script>
+    
+</script>
