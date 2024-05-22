c6c0aa9b697f0eaee221377622fb4b2b48487dc2
Simo64Terminator <simo64.tomasella@gmail.com>
2024-05-22 10:18:21 +0200

    boh

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@ import { createMemoryHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/views/Home.vue';
 import WhoWeAreView from '@/views/ChiSiamo.vue';
-import TermsOfService from '@/views/TermsOfService.vue';
+import TermsOfServiceView from '@/views/TermsOfService.vue';
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/chi-siamo', name: 'whoWeAre', component: WhoWeAreView },
-    { path: '/terms-of-service', name: 'termsOfService', component: TermsOfService },
+    { path: '/terms-of-service', name: 'termsOfService', component: TermsOfServiceView },
 ]
 
 const router = createRouter({
