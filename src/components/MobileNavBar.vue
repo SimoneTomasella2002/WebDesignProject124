12a5e82d5c44d877b10225c2db61847cd0f4b7aa
kevinslashgun <kevinbarragan1402@gmail.com>
2024-05-23 10:13:58 +0200

    Aggiunto tema, primo aggiustamento navbar mobile

--- src/components/MobileNavBar.vue
+++ src/components/MobileNavBar.vue
@@ -1,33 +1,12 @@
 <template>
-    <v-app-bar app color="primary" dark>
+  <v-app-bar color="primary">
+    <template v-slot:prepend>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Mobile Navbar</v-toolbar-title>
-  
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list>
-          <v-list-item @click="drawer = false">
-            <RouterLink to="/">
-              <v-list-item-title>Home</v-list-item-title>
-            </RouterLink>
-          </v-list-item>
-          <v-list-item @click="drawer = false">
-            <RouterLink to="/views/ChiSiamo.vue">
-              <v-list-item-title>About</v-list-item-title>
-            </RouterLink>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-app-bar>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { RouterLink } from 'vue-router'
-  
-  const drawer = ref(false)
-  </script>
-
-  <style scoped>
-
-  </style>
-  
+    </template>
+    <v-app-bar-title>
+      <RouterLink to="/">
+        <img src="@/assets/images/logotype.svg" alt="logo" height="50" />
+      </RouterLink>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
--- src/main.js
+++ src/main.js
@@ -12,10 +12,53 @@ import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import Router from './router/index'
 
+const myAccessibleTheme = {
+    dark: true, // Modalità scura predefinita
+    colors: {
+        background: '#f3efd4', // Colore primario (chiaro)
+        surface: '#e6e1c2', // Sfondo leggermente più scuro
+        'surface-bright': '#e6e1c2', // Sfondo leggermente più scuro
+        'surface-light': '#d9d2b4', // Sfondo ancora più chiaro
+        'surface-variant': '#424242', // Grigio scuro per elementi contrastanti
+        'on-surface-variant': '#EEEEEE', // Testo chiaro su elementi contrastanti
+        primary: '#f3efd4', // Colore primario (chiaro)
+        'primary-darken-1': '#e6e1c2', // Versione leggermente più scura del primario
+        secondary: '#48A9A6', // Colore secondario
+        'secondary-darken-1': '#018786', // Versione leggermente più scura del secondario
+        error: '#B00020', // Rosso per errori
+        info: '#2196F3', // Blu per informazioni
+        success: '#4CAF50', // Verde per successo
+        warning: '#FB8C00', // Giallo per avvertenze
+    },
+    variables: {
+        'border-color': '#000000',
+        'border-opacity': 0.12,
+        'high-emphasis-opacity': 0.87,
+        'medium-emphasis-opacity': 0.60,
+        'disabled-opacity': 0.38,
+        'idle-opacity': 0.04,
+        'hover-opacity': 0.04,
+        'focus-opacity': 0.12,
+        'selected-opacity': 0.08,
+        'activated-opacity': 0.12,
+        'pressed-opacity': 0.12,
+        'dragged-opacity': 0.08,
+        'theme-kbd': '#212529',
+        'theme-on-kbd': '#FFFFFF',
+        'theme-code': '#F5F5F5',
+        'theme-on-code': '#000000',
+    }
+}
+
 const vuetify = createVuetify({
     components,
     directives,
-    iconsets,
+    theme: {
+        defaultTheme: 'myAccessibleTheme',
+        themes: {
+            myAccessibleTheme,
+        },
+    },
 })
 
 createApp(App)
