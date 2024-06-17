e066146b205c768b5baba016c052b85812e36937
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-06-17 10:56:45 +0200

    Update DesktopMainContent.vue

--- src/views/DesktopMainContent.vue
+++ src/views/DesktopMainContent.vue
@@ -10,9 +10,9 @@ import EdgeCard from "@/components/Timeline/TimeLineItems/EdgeCard.vue";
     <h1 class="title">Età</h1>
     <div class="timeline-wrapper">
       <Timeline>
-        <TimelineRow :age="25">
+        <TimelineRow :age="5">
           <template v-slot:sx-edge-card>
-            <EdgeCard title="left edge card one" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perspiciatis error suscipit vitae " />
+            <EdgeCard title="left edge card one" text="Alessandro vive in un paese dell’entroterra ligure, in Italia, non molto distante dal capoluogo. Molto curioso, vivace, robusto." />
           </template>
           <template v-slot:sx-story-board>
             <StoryBoardCard title="left story board card one" />
@@ -21,12 +21,15 @@ import EdgeCard from "@/components/Timeline/TimeLineItems/EdgeCard.vue";
             <StoryBoardCard title="right story board card one" />
           </template>
           <template v-slot:dx-edge-card>
-            <EdgeCard title="right edge card one" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perspiciatis error suscipit vitae " />
+            <EdgeCard title="right edge card one" text="Tara vive in un paese sulle montagne dell’Afghanistan, nella regione del Ghazni. Giudiziosa, decisa, d’aspetto sobrio." />
           </template>
         </TimelineRow>
-        <TimelineRow :age="30">
+
+
+
+        <TimelineRow :age="6">
           <template v-slot:sx-edge-card>
-            <EdgeCard title="left edge card two" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perspiciatis error suscipit vitae " />
+            <EdgeCard title="left edge card two" text="Fa il primo viaggio all’estero con la famiglia in Spagna, la prima volta in aereo, è facile viaggiare in Europa." />
           </template>
           <template v-slot:sx-story-board>
             <StoryBoardCard title="left story board card two" />
@@ -35,7 +38,143 @@ import EdgeCard from "@/components/Timeline/TimeLineItems/EdgeCard.vue";
             <StoryBoardCard title="right story board card two" />
           </template>
           <template v-slot:dx-edge-card>
-            <EdgeCard title="right edge card two" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perspiciatis error suscipit vitae " />
+            <EdgeCard title="right edge card two" text="I suoi genitori hanno deciso di far studiare lei e i suoi fratelli, così comincia ad andare a scuola in città (zone di influenza e corridoi militari occidentali, sopprattutto USA) " />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="11">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Durante le scuole medie, si innamora della biologia e in futuro decide di iscriversi al liceo scientifico. I genitori lo mandano con amici ai campi estivi inglesi per imparare la lingua." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Mette il velo per la prima volta, come da tradizione della sua cultura quando si diventa donna, il velo non le copre mai i ciuffi più ribelli. " />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="18">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Durante il suo percorso di studio, ha modo di fare un anno di scambio culturale negli USA, prima volta che fa il passaporto, prima volta otreoceano. " />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Essendo in età da marito, si sposa con un uomo sempre originario delle sue zone, lui si è opposto ai talebani ed essendo emigrato da minorenne ha cittadinanza italiana." />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="19">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Consecuito il diploma, si ritrova a riflettere su dove continuare i suoi studi. Alla fine decide di specializzarsi in biochimica e fa richiesta alla Oxford University e dopo il colloquio viene preso." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Anche suo marito la esorta ad emanciparsi, così si iscrive all’università. Ma una voce però comincia a girare: gli americani voglio andarsene dal paese. " />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="20">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Dopo essersi trasferito nella città britannica giusto per il periodo di studio, incomincia a cercare un lavoretto part-time per poter aiutare economicamente ed essere più indipendente." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Tara è incinta, ma l’ombra dei talebani si fa sempre più presente. Bisogna dare un futro a questo bambino...bisogna andare via dal paese. Suo marito è tornato in Italia, lei nel frattempo si reca a Kabul, fa il passaporto e parte per l’Iran. " />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="21">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Il Regno Unito è cambiato dopo la Brexit ed è decisamente più difficile stare dietro alla burocrazia del paese, non riesce a trovare un occupazione che gli permetta di poter fare tutto, è la prima volta che si sente veramente “immigrato”." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="In Iran è ospitata da un parente che vive lì da tempo, attende il permesso di partire per raggiungere il marito, nel frattempo nasce il suo primo figlio, ora è mamma e si può solo andare avanti. " />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="22">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Durante un suo periodo a casa, mentre rinnova il suo vecchio passaporto (maggiorenne, 10 anni di durata) fa la conoscenza di quella che diventerà la sua fidanzata, è la sua prima storia d’amore più seria." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Finalmente arriva il permesso di ricongiungimento. Dopo tanta attesa, prende suo figlio in braccio e si incammina per l’Europa attraverso i canali umanitari della rotta balcanica." />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="23">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Sono anni di duro studio, alla fine del terzo anno, può scegliere di poter proseguire con una sua ricerca assistito da un professore. " />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Finalmente, al compimento dei suoi 23 anni, arriva in Italia e raggiunge suo marito nella sua nuova casa. Sà di essere più fortunata di molti altri, il suo prossimo obiettivo è integrarsi nel nuovo paese." />
+          </template>
+        </TimelineRow>
+
+
+
+        <TimelineRow :age="24">
+          <template v-slot:sx-edge-card>
+            <EdgeCard title="left edge card two" text="Dopo il periodo di ricerca pensa di tornare nell’Unione Europea per poter diventare ricercatore. Per quanto vorrebbe tornare in Italia, pensa che potrà andare avanti nella sua specializzazione solo in altri paesi UE per il momento." />
+          </template>
+          <template v-slot:sx-story-board>
+            <StoryBoardCard title="left story board card two" />
+          </template>
+          <template v-slot:dx-story-board>
+            <StoryBoardCard title="right story board card two" />
+          </template>
+          <template v-slot:dx-edge-card>
+            <EdgeCard title="right edge card two" text="Dopo un anno, riesce a parlare abbastanza bene la lingua e suo figlio può andare a scuola. Anche se sà che ci saranno ancora parecchie difficoltà nell’integrarsi, spera di poter riprendere a studiare lei stessa un giorno." />
           </template>
         </TimelineRow>
       </Timeline>
