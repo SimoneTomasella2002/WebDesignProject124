60ca8a9290e5f55e2816d6504d2d0083be073a04
kevinslashgun <kevinbarragan1402@gmail.com>
2024-06-18 20:14:42 +0200

    mobile footer work in progress

--- src/components/MobileFooter.vue
+++ src/components/MobileFooter.vue
@@ -3,5 +3,20 @@
 </script>
 
 <template>
-    
+    <v-container>
+        <v-row class="text-center">
+            <v-bottom-navigation>
+                <v-col>
+                    <v-btn class="h-100 text-none" variant="outlined" rounded>
+                        Passport1
+                    </v-btn>
+                </v-col>
+                <v-col>
+                    <v-btn class="h-100 text-none" variant="outlined" rounded>
+                        Passport2
+                    </v-btn>
+                </v-col>
+            </v-bottom-navigation>
+        </v-row>
+    </v-container>
 </template>
