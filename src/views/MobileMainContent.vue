b5dbdedfbfa7215b965122fbc75872424ec7df55
kevinslashgun <kevinbarragan1402@gmail.com>
2024-06-21 10:39:07 +0200

    swipe implementato

--- src/views/MobileMainContent.vue
+++ src/views/MobileMainContent.vue
@@ -23,11 +23,21 @@ const activeId = ref(0);
 const updateActiveId = (id) => {
     activeId.value == id ? activeId.value = 0 : activeId.value = id;
 }
+
+const swipeDirection = ref('None'); // Right => passport 1, Left => passport 2
+
+const swipe = (direction) => {
+    swipeDirection.value = direction;
+    console.log('Swipe direction: ', swipeDirection.value);
+}
+
+const name = computed(() => swipeDirection.value === 'Left' ? rightName.value : leftName.value);
+const side = computed(() => swipeDirection.value === 'Left' ? 'end' : 'start');
 </script>
 
 <template>
-    <v-window class="mb-4" color="background">
-        <v-timeline class="mb-2" side="start" line-color="red" line-thickness="30" density="comfortable"
+    <v-window class="mb-4" color="background" :touch="{left: () => swipe('Left'), right: () => swipe('Right')}">
+        <v-timeline class="mb-2" :side="side" line-color="red" line-thickness="30" density="comfortable"
             truncate-line="both">
             <v-timeline-item line-inset="1" fill-dot dot-color="background" size="45">
                 <template #icon>
@@ -36,8 +46,8 @@ const updateActiveId = (id) => {
                     </span>
                 </template>
             </v-timeline-item>
-            <v-timeline-item v-for="story in Stories[leftName]" :key="story.index" size="65">
-                <MobileCard :id="story.index" :description="story.description" :image="images[leftName + story.index]"
+            <v-timeline-item v-for="story in Stories[name]" :key="story.index" size="65">
+                <MobileCard :id="story.index" :description="story.description" :image="images[name + story.index]"
                     :show-text="activeId === story.index" @update:show-text="updateActiveId"></MobileCard>
                 <template #icon>
                     <span class="my-icon rounded-circle bg-red text-center">
