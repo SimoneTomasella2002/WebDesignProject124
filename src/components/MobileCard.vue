d485762d23dc523c75f17f0819e07c0c84c2d456
kevinslashgun <kevinbarragan1402@gmail.com>
2024-06-22 10:55:45 +0200

    card flip animation nel mobile

--- src/components/MobileCard.vue
+++ src/components/MobileCard.vue
@@ -14,16 +14,57 @@ const handleUpdate = () => {
 </script>
 
 <template>
-    <v-card class="rounded-xl overflow-hidden bg-primary" elevation="5" @click="handleUpdate">
-        <v-card-item class="ma-0 pa-0 rounded-xl">
-            <v-card-text v-if="showText" style="height: 30.608vh; width: 70vw;">
-                {{ props.description }}
-            </v-card-text>
-            <v-card-text v-else class="ma-0 pa-0 d-flex justify-center align-center rounded-xl">
-                <v-img class="ma-0 pa-0 d-flex justify-center align-center" :src="props.image" alt="Story Image"
-                    width="70vw">
-                </v-img>
-            </v-card-text>
-        </v-card-item>
-    </v-card>
+    <div class="card-wrapper">
+        <v-card class="rounded-xl overflow-hidden bg-primary flip-card" :class="{ 'is-flipped': showText }"
+            elevation="5" @click="handleUpdate">
+            <v-card-item class="ma-0 pa-0 rounded-xl">
+                <div class="card-front" v-show="!showText">
+                    <v-card-text class="ma-0 pa-0 d-flex justify-center align-center rounded-xl">
+                        <v-img class="ma-0 pa-0" :src="image" alt="Story Image" width="100%" height="100%"></v-img>
+                    </v-card-text>
+                </div>
+                <v-card-text class="card-back" v-show="showText" style="height: 30.608vh; width: 70vw;">
+                    {{ description }}
+                </v-card-text>
+            </v-card-item>
+        </v-card>
+    </div>
 </template>
+
+
+<style scoped>
+.card-wrapper {
+    perspective: 1000px;
+}
+
+.flip-card {
+    width: 70vw;
+    height: 30.608vh;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+}
+
+.flip-card.is-flipped {
+    transform: rotateY(180deg);
+}
+
+.card-front {
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-back {
+    transform: rotateY(180deg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
