eec329f4b838a339e6187f9f6ab75a0a024a72bc
kevinslashgun <kevinbarragan1402@gmail.com>
2024-06-27 17:35:57 +0200

    provo a disabilitare dialog in mobile

--- src/views/MobileMainContent.vue
+++ src/views/MobileMainContent.vue
@@ -45,7 +45,7 @@ function imageName(index) {
 
 <template>
     <v-window class="pa-4 d-flex justify-center align-center" color="background"
-        :style="{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed'}"
+        :style="{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover'}"
         :touch="{ left: () => swipe('Left'), right: () => swipe('Right') }" role="main">
         <v-timeline class="mb-4" :side="side" line-color="red" line-thickness="30" density="compact"
             truncate-line="both">
