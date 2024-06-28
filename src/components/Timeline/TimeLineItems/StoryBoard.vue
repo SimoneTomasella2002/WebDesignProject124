0047fbfa30efc788cedeb5d2e0a6641c610ed0c6
Loris Prataiolo <lorisprataiolo@outlook.com>
2024-06-28 11:38:54 +0200

    Update StoryBoard.vue

--- src/components/Timeline/TimeLineItems/StoryBoard.vue
+++ src/components/Timeline/TimeLineItems/StoryBoard.vue
@@ -1,14 +1,11 @@
 <script setup>
-
 import { ref } from 'vue';
 
 const isDialogActive = ref(false);
-
 </script>
 
-
 <template>
-  <v-dialog v-model="isDialogActive" max-width="900 " transition="scale-transition">
+  <v-dialog v-model="isDialogActive" max-width="900" transition="scale-transition">
     <template v-slot:activator="{ attrs }">
       <div @click="isDialogActive = true" class="story-board-card" v-bind="attrs" style="cursor: pointer">
         <v-img tabindex="0" aria-label="Story Board Card" :src="imageSrc" alt="Story Image" class="story-image" cover>
@@ -17,11 +14,11 @@ const isDialogActive = ref(false);
     </template>
 
     <template v-slot:default="{ isActive }">
-      <v-img tabindex="0" aria-label="Story Board Card" :src="imageSrc" alt="Story Image" class="story-image" cover>
-      </v-img>
-
+      <div class="story-board-card">
+        <v-img tabindex="0" aria-label="Story Board Card" :src="imageSrc" alt="Story Image" class="story-image" cover>
+        </v-img>
+      </div>
     </template>
-
   </v-dialog>
 </template>
 
