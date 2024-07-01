075c29729aabc5112ace3d3a053d97be8795b312
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2024-07-01 21:35:56 +0200

    fixed links in tos

--- src/views/TermsOfService.vue
+++ src/views/TermsOfService.vue
@@ -69,7 +69,9 @@ const language = computed(() => {
               }}
             </p>
             <a class="terms-of-service-text" href="https://www.tpi.it/news/joy-albina-africana-italia-20190305266193/"> {{ language === 'English' ? "Joy's story" : "Storia di Joy" }}</a>
+            <br>
             <a class="terms-of-service-text" href="https://iir.gmu.edu/immigrant-stories-dc-baltimore/korea/summary-of-interview-with-korean-immigrant"> {{ language === 'English' ? "Ji-hoon's story" : "Storia di Ji-hoon" }}</a>
+            <br>
             <a class="terms-of-service-text" href="nytimes.com/2015/08/02/fashion/a-kiss-deferred-by-bosnia-and-herzegovina-civil-war.html?_r=0"> {{ language === 'English' ? "Graphics inspiration" : "Ispirazione grafica" }}</a>
             <div class="text-subtitle-1">
               {{ language === 'English' ? "Font Used" : "Font Usato" }}
